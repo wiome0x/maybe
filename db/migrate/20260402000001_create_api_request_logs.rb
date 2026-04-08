46e03c189d94b9e41c49c68fae7762021255541e
zhengxuan.zhang <zhengxuan.zhang@hexagon.com>
2026-04-08 17:33:15 +0800

    style: fix rubocop indentation and array bracket spacing

--- db/migrate/20260402000001_create_api_request_logs.rb
+++ db/migrate/20260402000001_create_api_request_logs.rb
@@ -13,8 +13,8 @@ class CreateApiRequestLogs < ActiveRecord::Migration[7.2]
       t.timestamps
     end
 
-    add_index :api_request_logs, [:provider_name, :requested_at], name: "idx_api_request_logs_provider_requested_at"
-    add_index :api_request_logs, [:request_status], name: "idx_api_request_logs_status"
-    add_index :api_request_logs, [:requested_at], name: "idx_api_request_logs_requested_at"
+    add_index :api_request_logs, [ :provider_name, :requested_at ], name: "idx_api_request_logs_provider_requested_at"
+    add_index :api_request_logs, [ :request_status ], name: "idx_api_request_logs_status"
+    add_index :api_request_logs, [ :requested_at ], name: "idx_api_request_logs_requested_at"
   end
 end
